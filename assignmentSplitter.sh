332e2eedbc1d56dc08587da6af8d86e01c767a61
Paul Foster <paulfstr@gmail.com>
2013-09-06 15:18:11 +1000

    Add file splitting and renaming

--- assignmentSplitter.sh
+++ assignmentSplitter.sh
@@ -9,11 +9,18 @@ if [[ ! -f $outputFile ]]; then
   exit 1
 fi
 
-# Make/cd directory and move output file
-if [[ ! (mkdir $splitFolder) && (cp $outputFile $splitFolder/) && (cd $splitFolder) ]]; then
+# Make directory and move output file
+if ! (mkdir $splitFolder && cp $outputFile $splitFolder/$outputFile); then
   echo "Problem making directory or moving file." >&2
   exit 1
 fi
 
-# Split the file
+cd $splitFolder/
 split -p "final/main.cPage 1$" $outputFile
+
+i=1
+for f in x*; do
+  studUsername=`head -n 1 $f | sed 's|.*/\(.*\)/final/main.cPage 1$|\1|'`
+  mv $f "`printf '%02d' $i` $studUsername"
+  let "i += 1"
+done
