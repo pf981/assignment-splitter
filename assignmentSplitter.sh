dd767aa6d7f77bbb07ec4a62cdd90a117b7e64b7
Paul Foster <paulfstr@gmail.com>
2014-04-06 17:28:26 +1000

    WIP: Try to split multiple files

--- assignmentSplitter.sh
+++ assignmentSplitter.sh
@@ -1,26 +1,49 @@
 #!/bin/bash
+# FIXME: TODO: Make it run with $ assignmentSplitter CL01.txt CL02.txt CL04.txt    and it gives folders splitCL01 splitCL02 splitCL03
 
-outputFile='output.txt'
-splitFolder='splits'
+printUsage() {
+  echo "Usage: $0 [output1.txt] [output2.txt] ..."
+}
 
-# Ensure the file exists
-if [[ ! -f $outputFile ]]; then
-  echo "$outputFile not found." >&2
-  exit 1
-fi
+# Uses the file $1 to creat folder, splits$1, which contains the split files
+splitSingleFile() {
+  #outputFile='output.txt'
+  #splitFolder='splits'
+  outputFile=$1
+  splitFolder="splits$1"
 
-# Make directory and move output file
-if ! (mkdir $splitFolder && cp $outputFile $splitFolder/$outputFile); then
-  echo "Problem making directory or moving file." >&2
-  exit 1
-fi
+  # Ensure the file exists
+  if [[ ! -f $outputFile ]]; then
+    echo "$outputFile not found." >&2
+    exit 1
+  fi
 
-cd $splitFolder/
-split -p "final/main.c Page 1$" $outputFile
+  # Make directory and move output file
+  if ! (mkdir $splitFolder && cp $outputFile $splitFolder/$outputFile); then
+    echo "Problem making directory or moving file." >&2
+    exit 1
+  fi
 
-i=1
-for f in x*; do
-  studUsername=`head -n 1 $f | sed 's|.*/\(.*\)/final/main.c Page 1$|\1|'`
-  mv $f "`printf '%02d' $i` $studUsername"
-  let "i += 1"
-done
+  cd $splitFolder/
+  split -p "final/main.c Page 1$" $outputFile
+
+  i=1
+  for f in x*; do
+    studUsername=`head -n 1 $f | sed 's|.*/\(.*\)/final/main.c Page 1$|\1|'`
+    mv $f "`printf '%02d' $i` $studUsername"
+    let "i += 1"
+  done
+}
+
+main() {
+# Ensure at least one argument is given
+  [[ $# -gt 0 ]] || { printUsage; exit 1; }
+#if [ -z "$1" ]; then
+#  printUsage
+#fi
+
+# use $1 then go shift and repeat until $1 is empty
+  splitSingleFile $@
+}
+
+main "$@"
